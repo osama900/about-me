77a9eb453b1be3968b71910740464a0078f62287
osama900 <osamaos2233@gmail.com>
2021-06-25 01:07:17 +0300

    final editing

--- js/app.js
+++ js/app.js
@@ -2,230 +2,199 @@
 
 let feel;
 let score = 0;
-feel = confirm('H! I\'m Osama welcome to my page \n Feeeeeel GOOD ?');
+feel = confirm("H! I'm Osama welcome to my page \n Feeeeeel GOOD ?");
 
-// console.log(feel);
-if(feel == true){
-    alert('Thats Amazing ');
-}else{
-    alert('Hope you feel better ! ');
+if (feel == true) {
+  alert("Thats Amazing ");
+} else {
+  alert("Hope you feel better ! ");
 }
 
-
-
-//question 1 what is my name ---------------------------
-function osama(){
-let myName;
-myName = prompt('What is my name ? \n You can see it on the title  ');
-//console.log(myName);
-myName = myName.toLowerCase();
-//console.log(myName);
-while(myName !="osama" || myName == null){
-    myName = prompt(' What\'s My Name ? ');
-
-}
+function osama() {
+  let myName;
+  myName = prompt("What is my name ? \n You can see it on the title  ");
+  myName = myName.toLowerCase();
+  while (myName != "osama" || myName == null) {
+    myName = prompt(" What's My Name ? ");
+  }
 }
 osama();
-// question 2 what is guest name ---------------------------------
-function guest_name(){
-let gestName;
-gestName = prompt(' What\'s Your Name ? ');
+function guest_name() {
+  let gestName;
+  gestName = prompt(" What's Your Name ? ");
 
-while(gestName == null){
-    gestName = prompt(' What\'s Your Name ? ');
-
-}
-alert("nice to meet you"  +" "+gestName );
-//console.log(gestName);
-//console.log(typeof(gestName));
-//console.log("nice to meet you" + gestName);
+  while (gestName == null) {
+    gestName = prompt(" What's Your Name ? ");
+  }
+  alert("nice to meet you" + " " + gestName);
 }
 guest_name();
 
-function my_age(){
-//question 3 how old am i --------------------
-let age = prompt('do you think i\'m 33 years old ? \n answer with y/n or yes/no please ');
-//console.log(typeof(age));
-while(age == null){
-    age = prompt('do you think i\'m 33 years old ? \n answer with y/n or yes/no please ');
+function my_age() {
+  let age = prompt(
+    "do you think i'm 33 years old ? \n answer with y/n or yes/no please "
+  );
+  while (age == null) {
+    age = prompt(
+      "do you think i'm 33 years old ? \n answer with y/n or yes/no please "
+    );
+  }
 
+  switch (age.toLowerCase()) {
+    case "yes":
+      alert("almost guessed i'm 31 ");
+      break;
+    case "y":
+      alert("almost guessed i'm 31 ");
+      break;
+
+    case "no":
+      score++;
+      alert("right i'm 31 ");
+      break;
+
+    case "n":
+      score++;
+
+      alert("right guessed i'm 31 ");
+      break;
+  }
 }
+my_age();
+function fav_app() {
+  let favApp = prompt(
+    "do you think my favorite app is Youtube \n answer with y/n or yes/no please "
+  );
 
-    switch (age.toLowerCase()) {
-        case "yes":
-            alert('almost guessed i\'m 31 ') ;
-       break;
-        case 'y':
-            alert('almost guessed i\'m 31 ') ;
-        break;
+  switch (favApp.toLowerCase()) {
+    case "yes":
+    case "y":
+      score++;
 
-        case 'no':
-            score++;
-             alert('right i\'m 31 ') ;
-        break;
+      alert("You are right ");
+      break;
 
-        case 'n':
-            score++;
+    case "no":
+      alert("sorry it is");
+      break;
 
-             alert('right guessed i\'m 31 ') ;
-        break;
+    case "n":
+      alert("sorry it is ");
+      break;
+  }
+}
+fav_app();
 
-        
-        }
-    }my_age();   
-   function fav_app(){     
-//question 4 my favorite app  --------------------
+function my_exp() {
+  let experience = prompt("Do i have (4) apps on Google play store ? ");
 
-let favApp = prompt('do you think my favorite app is Youtube \n answer with y/n or yes/no please ');
+  switch (experience.toLowerCase()) {
+    case "yes":
+      score++;
 
-    switch (favApp.toLowerCase()) {
-        case "yes":
-        case 'y': 
-            score++;
+      alert("You are right ");
+      break;
+    case "y":
+      score++;
 
-            alert('You are right ') ;
-       break;
-        
-        case 'no':
-             alert('sorry it is') ;
-        break;
+      alert("You are right ");
+      break;
 
-        case 'n':
-             alert('sorry it is ') ;
-        break;
+    case "no":
+      alert("sorry i do");
+      break;
 
-        
-        }
-    }fav_app();
+    case "n":
+      alert("sorry i do ");
+      break;
+  }
+}
+my_exp();
 
-//question 5 experience app  --------------------
-  
-function my_exp(){
-let experience = prompt('Do i have (4) apps on Google play store ? ');
+function fav_food() {
+  let food = prompt("Do i love Maqloubeh ? ");
 
-    switch (experience.toLowerCase()) {
-        case "yes":
-            score++;
+  switch (food.toLowerCase()) {
+    case "yes":
+    case "y":
+      score++;
 
-            alert('You are right ') ;
-       break;
-        case 'y':
-            score++;
+      alert("You are right ");
+      break;
 
-            alert('You are right ') ;
-        break;
+    case "no":
+    case "n":
+      alert("sorry i do");
+      break;
+  }
+}
+fav_food();
 
-        case 'no':
-             alert('sorry i do') ;
-        break;
+function fav_car() {
+  let favCar = prompt("Do you think my favorite car is Tesla ? ");
 
-        case 'n':
-             alert('sorry i do ') ;
-        break;
+  switch (favCar.toLowerCase()) {
+    case "yes":
+    case "y":
+      score++;
 
-        
-        }
-    }my_exp();
+      alert("You are right it is tesla ");
+      break;
 
-    function fav_food(){
-//question 6 fav food  --------------------
-  
-
-let food = prompt('Do i love Maqloubeh ? ');
-
-    switch (food.toLowerCase()) {
-        case "yes":
-        case 'y':
-            score++;
-
-            alert('You are right ') ;
-       break;
-        
-        case 'no':
-        case 'n':
-             alert('sorry i do') ;
-        break;
-
-        
-        }
-
-    }fav_food();
-
-    function fav_car(){
-        //question 7 fav car  --------------------
-
-let favCar = prompt('Do you think my favorite car is Tesla ? ');
-
-    switch (favCar.toLowerCase()) {
-        case "yes":
-        case 'y':
-            score++;
-
-            alert('You are right it is tesla ') ;
-       break;
-        
-        case 'no':
-        case 'n':
-             alert('sorry it is') ;
-        break;
-        }
-
-    }fav_car();   
+    case "no":
+    case "n":
+      alert("sorry it is");
+      break;
+  }
+}
+fav_car();
 
 const myNum = 25;
 let gessNumber;
-function guss_number(){
-//console.log(gessNumber);
+function guss_number() {
+  for (let i = 0; i < 4; i++) {
+    gessNumber = parseInt(prompt("please insert a number "));
+    if (gessNumber == myNum) {
+      alert("your guess correct the number is 25");
+      score++;
 
-for (let i =0 ; i <4 ; i++){
-    //console.log(i);
-    gessNumber = parseInt(prompt('please insert a number '));
-    if(gessNumber == myNum ){
-        //console.log('your guess correct ');
-        alert('your guess correct the number is 25');
+      break;
+    } else if (gessNumber > myNum) {
+      alert("too heigh !");
+      continue;
+    } else {
+      alert("too low !");
+      continue;
+    }
+  }
+  alert(" number is 25");
+}
+guss_number();
+
+function where_living() {
+  let living = ["jordan", "karak", "aqaba"];
+
+  for (let x = 0; x < 6; x++) {
+    let city = prompt("which city do you think i live in ", " not amman :)");
+    for (let xx = 0; xx < living.length; xx++)
+      if (city == living[xx]) {
+        alert("you guessed the city");
         score++;
 
-        break;
-    }
-    else if (gessNumber > myNum){
-        //console.log('too high ! ');
-        alert('too heigh !')
-        continue;
-    }
-    else{
-        alert('too low !')
-        //console.log('too low ! ');
-        continue;
-    }
+        x = 6;
+      }
+  }
 }
-alert(' number is 25');
+where_living();
 
-}guss_number();
-
-function where_living(){
-let living = ['jordan','karak','aqaba'];
-
-for (let x =0 ; x <6 ; x++){
-     let city=prompt('which city do you think i live in ',' not amman :)')
-     for (let xx = 0 ; xx < living.length ; xx++)
-        if (city == living[xx]){
-            alert('you guessed the city');
-            score++;
-
-            x=6;
-            //break;
-        }
-        
-       
-    //console.log(living);
-    //console.log(city);
-
-}
-}where_living();
-
-
-
-
-
-alert("it's pleasure to meet you"+ " " + gestName + 'and scored '+ " " + score + " "+"out of 7"+ "\n hope to see you soon and please enjoy in my page ");
-
+alert(
+  "it's pleasure to meet you" +
+    " " +
+    gestName +
+    "and scored " +
+    " " +
+    score +
+    " " +
+    "out of 7" +
+    "\n hope to see you soon and please enjoy in my page "
+);
